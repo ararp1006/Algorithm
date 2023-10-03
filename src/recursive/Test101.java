d78078423257ed1582a4d7801bf27d0b4005ef3f
ararp1006 <ararp1006@gmail.com>
2023-10-03 23:39:15 +0900

    재귀1

--- src/recursive/Test101.java
+++ src/recursive/Test101.java
@@ -6,9 +6,8 @@ public class Test101 {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             System.out.print("숫자를 입력하세요: ");
-            int num = scanner.nextInt();
-            scanner.close();
 
+            int num = scanner.nextInt();
             int result = sumTo(num);
             System.out.println("1부터 " + num + "까지의 합: " + result);
         }
