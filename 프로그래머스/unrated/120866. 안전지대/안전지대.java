55b430676ddb19be10cb3f130c574df23bb8d120
ararp1006 <130068083+ararp1006@users.noreply.github.com>
2023-10-17 00:07:49 +0900

    [level unrated] Title: 안전지대, Time: 0.03 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120866. 안전지대/안전지대.java
@@ -0,0 +1,25 @@
+class Solution {
+	public int solution(int[][] board) {
+		int [][]list=new int [board.length+2][board.length+2];
+		int answer =0;
+		for(int i=0; i<board.length;i++) {
+			for(int y=0; y<board.length;y++) {
+				if(board[i][y]==1) {
+					for(int a=(i-1)+1;a<=(i+1)+1; a++) {
+						for(int b=(y-1)+1; b<=(y+1)+1; b++) {
+							if(list[a][b]!=1) {
+								list[a][b]=2;
+							}
+						}
+					}
+				}
+			}
+		}
+		for(int i=1; i<list.length-1;i++) {
+			for(int y=1;y<list.length-1;y++) {
+				if(list[i][y]==0) {answer++;}
+			}
+		}
+		return answer;
+	}
+}
