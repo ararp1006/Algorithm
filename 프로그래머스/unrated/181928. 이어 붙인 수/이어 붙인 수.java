1f74511b31562fba7203f0d99c1d06d53ed678ac
ararp1006 <130068083+ararp1006@users.noreply.github.com>
2023-11-05 22:43:18 +0900

    [level unrated] Title: 이어 붙인 수, Time: 9.01 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,25 +1,20 @@
-public class Solution {
-    public long solution(int[] num_list) {
-        StringBuilder oddStr = new StringBuilder();
-        StringBuilder evenStr = new StringBuilder();
-        
-        // 리스트의 각 숫자에 대해 반복합니다.
-        for (int num : num_list) {
-            // 홀수인 경우
-            if (num % 2 == 1) {
-                oddStr.append(num);
-            }
-            // 짝수인 경우
-            else {
-                evenStr.append(num);
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+
+        String even = "";
+        String odd = "";
+
+        for (int num:num_list) {
+            if(num % 2 == 0) {
+                even += num;
+            } else {
+                odd += num;
             }
         }
-        
-        // 홀수와 짝수 문자열을 정수로 변환하고 합산합니다.
-        // 만약 문자열이 비어있다면 0으로 처리합니다.
-        long oddNum = (oddStr.length() > 0) ? Long.parseLong(oddStr.toString()) : 0;
-        long evenNum = (evenStr.length() > 0) ? Long.parseLong(evenStr.toString()) : 0;
-        
-        return oddNum + evenNum;
+
+        answer = Integer.valueOf(even) + Integer.valueOf(odd);
+
+        return answer;
     }
 }
