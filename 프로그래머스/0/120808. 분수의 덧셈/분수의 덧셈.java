78430f1ce206a6abe98d1aece45c8aecfd81a999
ararp1006 <130068083+ararp1006@users.noreply.github.com>
2024-02-17 23:45:36 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.11 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,27 +1,18 @@
 class Solution {
-    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-      
-               
-        int commonNumer1 = numer1 * denom2; 
-        int commonNumer2 = numer2 * denom1;
-        int commonNumer = commonNumer1+commonNumer2; 
-        int commonDen = denom1 * denom2; 
-        
-        int gcd = 0;
-        
-   
-        for(int i=1; i <= commonNumer && i <= commonDen  ;i++){
-            if( commonNumer%i == 0 && commonDen%i ==0 ){
-                gcd = i;
+    public int[] solution(int denum1, int num1, int denum2, int num2) {
+        int[] answer = new int[2];
+
+        answer[0]=denum1*num2+denum2*num1;
+        answer[1]=num1*num2;
+        int min = Math.min(answer[0],answer[1]);
+        int max = 1;
+        for(int i=1; i<=min; i++){
+            if(answer[0]%i==0 && answer[1]%i==0){
+                max= i;
             }
         }
-        
-        commonNumer = commonNumer/gcd; 
-        commonDen = commonDen/gcd;
-        
-        int[] answer = {commonNumer, commonDen};
-        
-        
+        answer[0] /= max;
+        answer[1] /= max;
         return answer;
     }
 }
