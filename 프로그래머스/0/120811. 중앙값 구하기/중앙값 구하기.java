0132f5f9832ceb064cbeab376b2bc6ae78b69529
ararp1006 <130068083+ararp1006@users.noreply.github.com>
2024-03-27 23:07:48 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.39 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,18 +1,13 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class Solution {
-    public static double solution(int[] array) {
-        // 주어진 배열을 정렬
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+
         Arrays.sort(array);
-        int length = array.length;
-        
-        // 배열의 길이가 홀수인 경우 중앙값은 가운데 값
-        if (length % 2 == 1) {
-            return array[length / 2];
-        } else { 
-            // 배열의 길이가 짝수인 경우 중앙에 위치한 두 값의 평균을 반환
-            int midIndex = length / 2;
-            return (array[midIndex - 1] + array[midIndex]) / 2.0;
-        }
+
+        answer = array[array.length/2];
+
+        return answer;
     }
 }
